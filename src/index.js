8ecf2aedb211f868ee1a6fac7c4b75ffa2061610
surajith <surajithreddy@gmail.com>
2018-11-30 23:30:51 +0530

    Adding react app to display comments under blog

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App= (props) => {
+        
+    return(
+        <div className="ui container comments">
+            <div className="comment">
+               <a href="/" className="avatar"><img src={props.image} alt={props.imgname} height="100px" width="200px"/></a>
+               <div className="content">
+                   <a href="/" className="author">{props.authname}</a>
+               
+               <div className="metadata">
+                    <span  className="date">Today at {props.time}</span>
+               </div>
+               <div className="text">{props.comment}</div>
+               </div>
+            </div>
+        </div>
+
+
+    );
+}
+
+const Approvecard=(props)=>{
+    console.log(props.children)
+    return(
+        <div className="ui card">
+            <div className="content">{props.children}</div>
+            <div className="extra Content">
+                <div>
+                   <div className="ui basic green button">Accept</div>
+                   <div className="ui basic red button">Button</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Blog=()=>{
+    return(
+        <div>
+             <Approvecard><App authname="Surajith "time="6:00PM" comment="No Comments"  image="https://www.w3schools.com/css/img_5terre.jpg" imgname="House"/></Approvecard>
+             <Approvecard><App authname="Vijay" time="7:00PM" comment="I liked it very much" image="https://www.w3schools.com/css/img_forest.jpg" imgname="Forest"/></Approvecard>
+             <Approvecard><App authname="Viswa" time="12:00PM" comment="Pretty Impressive" image="https://www.w3schools.com/css/img_mountains.jpg" imgname="Mountains"/></Approvecard>
+        </div>
+       
+    );
+}
+ReactDOM.render(<Blog/>,document.getElementById("root"))
